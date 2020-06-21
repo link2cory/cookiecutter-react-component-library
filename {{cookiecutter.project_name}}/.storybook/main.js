a81718ed935a2dc1ed2631e90841869a33f92e5d
Cory Perkins <link2cory@gmail.com>
2020-06-21 15:45:44 -0700

    fixed the story file finding regex

--- {{cookiecutter.project_name}}/.storybook/main.js
+++ {{cookiecutter.project_name}}/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.[tjs]sx'],
+  stories: ['../src/**/*.stories.[tj]sx'],
   addons: [
     '@storybook/preset-typescript',
     '@storybook/addon-actions',
